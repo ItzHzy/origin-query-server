5c10547a1e021f071ff8b88f0304b215a2ff81ca
0xBC9 <admin@0xbc9.tech>
2020-07-17 20:10:49 -0500

    merged abilities into gameElements

--- lib/gameElements.py
+++ lib/gameElements.py
@@ -586,3 +586,39 @@ class Tracker():
                 self.x += self.increment
             else:
                 self.x = True  
+
+class Ability():
+    def __init__(self, game, source, allowedZones, rulesText, isManaAbility, keywordName):
+        self.source = source
+        self.rulesText = rulesText
+        self.keywordName = keywordName
+        self.abilityID = 'A-' + str(uuid1())
+        self.isManaAbility = isManaAbility
+        self.allowedZones = allowedZones
+        self.specialTypes = set() # Ex. Variable
+        game.GAT[self.abilityID] = self
+
+class ActivatedAbility(Ability):
+    def __init__(self, game, source, cost, effect, allowedZones, rulesText, isManaAbility=False, keywordName=None):
+        super(ActivatedAbility, self).__init__(game, source, allowedZones, rulesText, isManaAbility, keywordName)
+        self.cost = cost # cost[0] is a mana cost, cost[1] is for other costs
+        self.effect = effect
+        self.isActive = False 
+
+    def getCost(self):
+        return self.cost
+
+    def getEffect(self):
+        return self.effect
+
+
+class TriggeredAbility(Ability):
+    # argDict {1: None, 
+    #          3: self }
+    #   check the arg at index in the evaluated function and see if its equal to the value 
+    def __init__(self, game, source, triggerFunction, argDict, effect, allowedZones, rulesText, isManaAbility=False, keywordName=None):
+        super(TriggeredAbility, self).__init__(game, source, allowedZones, rulesText, isManaAbility, keywordName)
+        self.triggerFunction = triggerFunction
+        self.argDict = argDict
+        self.effect = effect
+        self.isActive = False
